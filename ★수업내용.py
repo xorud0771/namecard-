03dac0e7dfb560449d72d7ff8cf970d12a18f4eb
hanna9987 <wounhan@naver.com>
2025-03-10 17:41:28 +0900

    수업내용 수정 완성

--- ★수업내용.py
+++ ★수업내용.py
@@ -10,7 +10,7 @@ display = '''
 메뉴를 선택하세요 >>> '''
 
 menu = ''
-cards = []
+cards = [['하하', '010', '학교', 'gkssk'], ['파파', '098', '직장', 'qppq']]
 while True:
     menu = input(display)
     # 1번 선택: 명함 입력
@@ -35,7 +35,7 @@ while True:
         card = [name, phone, affiliation, email]
         cards.append(card)
         print("\n명함이 저장되었습니다.\n")
-        print(card)
+        print('저장된 명함 목록: ', cards)
         
     elif menu == '2':
         if len(cards) == 0:
@@ -43,7 +43,7 @@ while True:
             continue
 
         print("\n[ 명함 수정 ]")
-        named = input("수정할 명함의 이메일을 입력해주세요: ")
+        email = input("수정할 명함의 이메일을 입력해주세요: ")
         check = 0
         for index in range(len(cards)):
         # for index, card in enumerate(cards): (이것도 가능/ 결과값: (인덱스값, 실제값))
@@ -51,34 +51,33 @@ while True:
             if cards[index][3] == email:
                 check = 1
                 while True:
-                    item = input('수정할 항목을 선택하세요. \n1.이름 2.번호 3.소속 4.종료 >>> ')
+                    item = input('\n수정할 항목을 선택하세요. \n1.이름 2.번호 3.소속 4.종료 >>> ')
                     if item == '4':
                         break
                     item = int(item)
                     if item in (1,2,3):
-                        cards[index][item-1] = input('수정할 내용 입력 >>> ')
+                        cards[index][item-1] = input('\n수정할 내용 입력 >>> ')
                     if check == 0:
-                        print('데이터가 없습니다.')
-            if card[0] == named:
-                print("\n1. 이름 2. 이메일 3. 전화번호 4. 소속")
-                field = input("수정할 항목을 선택해주세요: ")
+                        print('잘못된 입력입니다. 다시 선택해주세요.')
 
     elif menu == '3':
         print("\n[ 명함 삭제 ]")
-        email = input('삭제할 명함의 이메일을 입력하세요. ')
+        email = input('삭제할 명함의 이메일을 입력하세요 : ')
         check = 0
         for index in range(len(cards)):
             if cards[index][3] == email:
                 check = 1
                 print('삭제 >>> ', cards.pop(index))
-                break
-            if check == 0:
-                print('입력된 명함이 없습니다.')
+                break # 찾았으면 삭제 후 반복문 종료
+        if check == 0:
+            print('입력된 명함이 없습니다.')
 
     elif menu == '4':
         print("\n[ 명함 목록 보기 ]")
+        print("-" * 60)
         for card in cards:
-            print(f'{card[0]} {card[1]} {card[2]} {card[3]}')
+            print(f'{card[0]} /  {card[1]} /  {card[2]} /  {card[3]}')
+        print("-" * 60)
 
     elif menu == '5':
         print("\n[ 프로그램 종료 ]")
